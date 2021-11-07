40b070f6a0c59e3e61fa0234491a674d566aedac
Jerzy Supryn (GeSup) <skuter6@interia.pl>
2021-11-07 22:59:45 +0100

    chapter 5 add fibonacci func

--- chapter-5/fibonacci/starter-code/index.js
+++ chapter-5/fibonacci/starter-code/index.js
@@ -1,7 +1,15 @@
-function fibonacci(num) {
+function fibonacci(num, rem) {
   // write your  code here
+  rem = rem || {};
+  if (rem[num]) return rem[num];
+  if (num > 1) {
+    return rem[num] = fibonacci(num - 1, rem) + fibonacci(num - 2, rem)
+  } else {
+    return 1
+  }
 }
 
 let response = prompt("How many numbers?")
 
 // how do you deal with calling the function and its output?
+alert(`The Fibonacci number is ${fibonacci(response)}`)
