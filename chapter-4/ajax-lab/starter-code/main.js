03de9d27c8d13d38406e082f752a5ef8f9ee14f2
Jerzy Supryn (GeSup) <skuter6@interia.pl>
2021-11-07 15:35:36 +0100

    my solution in chapter 4

--- chapter-4/ajax-lab/starter-code/main.js
+++ chapter-4/ajax-lab/starter-code/main.js
@@ -6,7 +6,18 @@ let hello = "Hello! My name is {name} and I'm from {planet}. I've been in {films
 
 fetch('https://swapi.dev/api/people/1/')
   // write your code here.
-  .then((response) => {
-    hello = hello.replace('{name}', name).replace('{planet}', planet).replace('{films}', filmNames)
-    document.querySelector('#main').innerHTML = hello
+  .then(response => response.json())
+  .then(pejson => {
+    name = pejson.name;
+    fetch(pejson.homeworld).then(response => response.json()).then(plJson => {
+      planet = plJson.name
+      for (const film of pejson.films.sort()) {
+        films.push(fetch(film).then(response => response.json()).then(fJson => fJson.title))
+      }
+      Promise.all(films).then((response) => {
+        filmNames = response.join(', ')
+        hello = hello.replace('{name}', name).replace('{planet}', planet).replace('{films}', filmNames)
+        document.querySelector('#main').innerHTML = hello
+      })
+    })
   })
