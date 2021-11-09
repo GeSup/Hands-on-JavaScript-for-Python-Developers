475a4c51c56ce620e6a7a5999a9c3a87e20483ba
Jerzy Supryn (GeSup) <skuter6@interia.pl>
2021-11-09 19:34:57 +0100

    chapter 6 stickies my solution

--- chapter-6/stickies/starter-code/script.js
+++ chapter-6/stickies/starter-code/script.js
@@ -2,17 +2,21 @@ const container = document.querySelector('.container') // set .container to a va
 let noteCount = 1 // inital value
 
 // access our button and assign a click handler
-
+document.querySelector('.box-creator-button').addEventListener('click',function(e){
   //  create our DOM element
-
+const sticky = document.createElement('div')
   // set our class name
-
+sticky.className = 'box'
   // get our other DOM elements
-
+const inputs = document.querySelectorAll('input')
   // get our variables
-
+const color = inputs[0].value
+const text = inputs[1].value
   // blank out the input fields
-
+inputs.forEach(input => {input.value = ''}) 
   // define the attributes
-
+sticky.style.backgroundColor = color
+sticky.innerHTML = noteCount++ + '. ' + text
   // add the sticky
+container.appendChild(sticky)
+})
